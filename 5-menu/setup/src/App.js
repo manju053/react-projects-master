7dbe1aa6355e4962b682bdb0a5977aa0edaf869f
smanjunatha5 <smanjunatha000@gmail.com>
2020-12-21 15:45:00 +0530

    menu items project completed

--- 5-menu/setup/src/App.js
+++ 5-menu/setup/src/App.js
@@ -4,7 +4,30 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(['All', ...(new Set(menuItems.map(menuItem => menuItem.category)))]);
+
+  const filterItems = (category) => {
+    if (category === 'All') {
+      setMenuItems(items);
+    } else {
+      const newItems = items.filter(menuItem => category === menuItem.category);
+      setMenuItems(newItems);
+    }
+
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+          <Categories filterItems={filterItems} categories={categories} />
+          <Menu items={menuItems} />
+        </div>
+      </section>
+    </main>
+  )
 }
 
 export default App;
