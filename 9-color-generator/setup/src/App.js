14a59b8b53f796b2d6d34fbcdd2d7ad5319056be
smanjunatha5 <smanjunatha000@gmail.com>
2020-12-28 14:37:23 +0530

    color generator project completed

--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -4,7 +4,43 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#f15025').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+    }
+
+  };
+  return (
+    <>
+      <section className="container">
+        <h4>color generator</h4>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder="#f15025" 
+          className={`${error && 'error'}`}/>
+          <button type="submit" className="btn">submit</button>
+        </form>
+      </section>
+
+      <section className="colors">
+        {
+          list.map((color, index) => {
+            const hex = color.hex;
+            return (
+              <SingleColor key={index} {...color} index={index} hexColor={hex}/>
+            )
+          })
+        }
+      </section>
+    </>
+  )
 }
 
 export default App
--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,31 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb, weight, index, hexColor}) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(',');
+
+  const copyClipboard = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(hexColor);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false)
+    }, 3000);
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [alert])
+  return (
+    <article className={`color ${index > 10 ? 'color-light' : null}`} style={{backgroundColor: `rgb(${bcg})`}}
+    onClick={() => copyClipboard()}>
+      <p className="percent-value">{weight}%</p>
+      <p className="color-value">#{hexColor}</p>
+      {alert && <p className="alert">copied to clipboard</p>}
+    </article>
+  )
 }
 
 export default SingleColor
