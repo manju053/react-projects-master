19a274a434b22d2c92e36b9e604748a647d0bd0a
smanjunatha5 <smanjunatha000@gmail.com>
2021-01-05 15:58:16 +0530

    cocktail project completed

--- 15-cocktails/setup/src/context.js
+++ 15-cocktails/setup/src/context.js
@@ -5,7 +5,47 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('a');
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchDrinks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${url}${searchTerm}`);
+      const data = await response.json();
+      console.log(data);
+      const { drinks } = data;
+      if(drinks) {
+        const newCockTails = drinks.map(drink => {
+          const {idDrink, strDrink, strDrinkThumb, strAlchoholic, strGlass} = drink;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlchoholic,
+            glass: strGlass
+          }
+        })
+        setCocktails(newCockTails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false)
+      
+    }
+  }, [searchTerm])
+
+  useEffect(() => {
+    fetchDrinks();
+  }, [searchTerm, fetchDrinks])
+  return <AppContext.Provider value={{
+    loading, searchTerm, cocktails, setSearchTerm
+  }}>
+    {children}
+  </AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus architecto autem harum! Adipisci praesentium iusto nisi tenetur hic quas labore deserunt officiis porro, voluptatum illo in facere et, quam necessitatibus, dolorum ea dicta minus omnis error. Tempore aliquid aut molestias.
+      </p>
+    </section>
   )
 }
 
